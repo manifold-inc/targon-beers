05e6fe1e3c0842a8b3f1f519054fa60355a97e16
darwich6 <adarwich2010@gmail.com>
2025-03-17 15:15:49 -0500

    fix: default mysql settings

--- modelo/main.go
+++ modelo/main.go
@@ -33,11 +33,6 @@ func init() {
 		)
 	}
 
-	// connection pool
-	db.SetMaxOpenConns(25)
-	db.SetMaxIdleConns(10)
-	db.SetConnMaxLifetime(5 * time.Minute)
-
 	// timeout
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
